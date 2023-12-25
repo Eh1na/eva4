065fe56544f63e555db1dc964b70fe783a727a1d
Javier Bobadilla <ja.bobadillam@gmail.com>
2023-12-25 18:51:18 -0300

    commin

--- guardar_datos.php
+++ guardar_datos.php
@@ -24,7 +24,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Verificar la conexión
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    die("Error de conexión: " . $conn->connect_error); 
 }
 
 
