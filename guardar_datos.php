eee09cffddf2a0a60d9ba04d889bd186f3c5ebb3
Javier Bobadilla <ja.bobadillam@gmail.com>
2023-12-25 18:58:17 -0300

    ok

--- guardar_datos.php
+++ guardar_datos.php
@@ -3,16 +3,9 @@
 $jsonData = $_POST['data'];
 $data = json_decode($jsonData, true);
 
-
 // Acceder a las propiedades y arreglos de 'data'
 $totalFabrica = $data['total_fabrica'];
 $totalBodega = $data['total_bodega'];
-$idMetodo = $data['id_metodo'];
-$costoTotal = $data['costo_total'];
-// $bodegas = $data['bodegas'];
-// $fabricas = $data['fabricas'];
-// $asignaciones = $data['asignacion'];
-// $costos = $data['costo'];
 
 // Realizar la conexión a la base de datos (reemplaza con tus propios datos)
 $servername = "localhost";
@@ -24,10 +17,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Verificar la conexión
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error); 
+    die("Error de conexión: " . $conn->connect_error);
 }
-
-
 
 // Insertar datos en la tabla 'problema'
 $sqlProblema = "INSERT INTO problema (total_fabrica, total_bodega) VALUES ('$totalFabrica', '$totalBodega')";
@@ -37,10 +28,9 @@ if ($conn->query($sqlProblema) === TRUE) {
     $idProblemaInsertado = $conn->insert_id;
 
     // Puedes continuar realizando más operaciones aquí si es necesario
-    
 
     // Enviar una respuesta de vuelta si es necesario
-    echo json_encode(['success' => 'Datos recibidos y procesados correctamente. ID del último registro insertado en \'problema\': ' . $idProblemaInsertado]);
+    echo json_encode(['success' => 'Datos recibidos y procesados correctamente.', 'id_problema_insertado' => $idProblemaInsertado]);
 } else {
     echo json_encode(['error' => 'Error al realizar la inserción en \'problema\': ' . $conn->error]);
 }
